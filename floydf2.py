15915917af36ddaa1c7dbe6ead1804dfeaeb7842
Paul Crowley <paulcrowley@google.com>
2018-10-15 06:18:11 -0700

    f2 doesn't guarantee anything wrt order.

--- floydf2.py
+++ floydf2.py
@@ -1,7 +1,7 @@
 import random
 
 def choose(n, k):
-    "k distinct integers 0 <= x < n, random"
+    "k distinct integers 0 <= x < n"
     d = set()
     for i in range(n - k, n):
         j = random.randrange(i+1)
@@ -9,8 +9,4 @@ def choose(n, k):
             d.add(i)
         else:
             d.add(j)
-    d = list(d)
-    for i in range(k):
-        j = random.randrange(i+1)
-        d[i], d[j] = d[j], d[i]
-    return d
+    return list(d)
