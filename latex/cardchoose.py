1c9511c5332e5a471329068bdc73f943c2776ec4
Paul Crowley <paulcrowley@google.com>
2022-11-20 19:45:02 -0800

    latex: Many cleanups

---
+++ latex/cardchoose.py
@@ -0,0 +1,14 @@
+def sorted_choose(n, k):
+    "k distinct integers 0 <= x < n, sorted"
+    t = n - k + 1
+    d = [None] * k
+    for i in range(k):
+        r = random.randrange(t + i)
+        if r < t:
+            d[i] = r
+        else:
+            d[i] = d[r - t]
+    d.sort()
+    for i in range(k):
+        d[i] += i
+    return d
